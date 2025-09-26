e9b599a7403df1fdf3d3fbae14826250ae8b51a6
Ana Helena <anahelenarp@hotmail.com>
2025-09-26 18:24:56 -0300

    feat: input text component

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -5,8 +5,9 @@ import {
   useFonts,
 } from '@expo-google-fonts/host-grotesk';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
+import { AppText } from './components/AppText';
 import { theme } from './theme';
 
 export function App() {
@@ -22,7 +23,7 @@ export function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: theme.fontSize['3xl'], fontFamily: theme.fontFamily.sans.semiBold }}>Open up App.tsx to start working on your app!</Text>
+      <AppText style={{ textAlign: 'right' }}>Open up App.tsx to start working on your app!</AppText>
       <StatusBar style="auto" />
     </View>
   );
@@ -32,7 +33,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.lime[500],
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ src/ui/components/AppText.tsx
@@ -0,0 +1,31 @@
+import { theme } from '@ui/theme';
+import React from 'react';
+import { Text } from 'react-native';
+
+interface IAppTextProps extends React.ComponentProps<typeof Text> {
+  size?: keyof typeof theme.fontSize;
+  fontFamily?: keyof typeof theme.fontFamily;
+  weight?: keyof typeof theme.fontFamily.sans;
+  color?: string;
+}
+
+export function AppText({
+  size = 'base',
+  fontFamily = 'sans',
+  weight = 'regular',
+  color = theme.colors.black[700],
+  style,
+  ...props
+}: IAppTextProps) {
+  return <Text
+    style={[
+      {
+        fontFamily: theme.fontFamily[fontFamily][weight],
+        fontSize: theme.fontSize[size],
+        color,
+      },
+      style,
+    ]}
+    {...props}
+  />;
+}
