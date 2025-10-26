25f3c18572c104eb8318b2c449e60b11e7f0bf30
Ana Helena <anahelenarp@hotmail.com>
2025-10-26 10:25:54 -0300

    feat: steps components

---
+++ src/ui/screens/Onboarding/components/Step/index.tsx
@@ -0,0 +1,62 @@
+import { View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { AppText } from '@ui/components/AppText';
+import { theme } from '@ui/styles/theme';
+import { styles } from './styles';
+
+export function Step({ children }: { children: React.ReactNode; }) {
+  const { bottom } = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.container, { paddingBottom: bottom }]}>
+      {children}
+    </View>
+  );
+}
+
+export function StepHeader({ children }: { children: React.ReactNode; }) {
+  return (
+    <View style={styles.header}>
+      {children}
+    </View>
+  );
+}
+
+export function StepTitle({ children }: { children: string; }) {
+  return (
+    <View style={styles.header}>
+      <AppText
+        size='3xl'
+        weight='semiBold'
+        style={styles.title}
+      >
+        {children}
+      </AppText>
+    </View>
+  );
+}
+
+export function StepSubtitle({ children }: { children: string; }) {
+  return (
+    <AppText color={theme.colors.gray[700]} style={styles.subTitle}>
+      {children}
+    </AppText>
+  );
+}
+
+export function StepContent({ children }: { children: React.ReactNode; }) {
+  return (
+    <View style={styles.content}>
+      {children}
+    </View>
+  );
+}
+
+export function StepFooter({ children }: { children: React.ReactNode; }) {
+  return (
+    <View style={styles.footer}>
+      {children}
+    </View>
+  );
+}
--- src/ui/screens/Onboarding/steps/GoalStep.tsx
+++ src/ui/screens/Onboarding/steps/GoalStep.tsx
@@ -1,20 +1,32 @@
-import { View } from 'react-native';
+import { ArrowRightIcon } from 'lucide-react-native';
+import React from 'react';
 
 import { AppText } from '@ui/components/AppText';
 import { Button } from '@ui/components/Button';
+import { theme } from '@ui/styles/theme';
+import { Step, StepContent, StepFooter, StepHeader, StepSubtitle, StepTitle } from '../components/Step';
 import { useOnboarding } from '../context/useOnboarding';
 
 export function GoalStep() {
-  const { currentStepIndex, nextStep, previousStep } = useOnboarding();
+  const { nextStep } = useOnboarding();
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppText size="3xl" weight="semiBold">Goal Screen</AppText>
+    <Step>
+      <StepHeader>
+        <StepTitle>Qual é seu objetivo?</StepTitle>
+        <StepSubtitle>O que você pretende alcançar com a dieta?</StepSubtitle>
+      </StepHeader>
 
-      <View>
-        <Button onPress={previousStep}>Voltar</Button>
-        <AppText>{currentStepIndex}</AppText>
-        <Button onPress={nextStep}>Avançar</Button>
-      </View>
-    </View>
+      <StepContent>
+        <AppText>
+          Step content
+        </AppText>
+      </StepContent>
+
+      <StepFooter>
+        <Button size='icon' onPress={nextStep}>
+          <ArrowRightIcon size={20} color={theme.colors.black[700]} />
+        </Button>
+      </StepFooter>
+    </Step>
   );
 }
