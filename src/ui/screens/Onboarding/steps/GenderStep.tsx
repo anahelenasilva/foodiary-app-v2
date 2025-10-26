0017e6262c6daa915cd6ef0b4670c6d30ce967ef
Ana Helena <anahelenarp@hotmail.com>
2025-10-26 19:04:31 -0300

    feat: gender step

--- src/ui/screens/Onboarding/steps/GenderStep.tsx
+++ src/ui/screens/Onboarding/steps/GenderStep.tsx
@@ -1,21 +1,44 @@
+import { ArrowRightIcon } from 'lucide-react-native';
 import React from 'react';
-import { View } from 'react-native';
 
-import { AppText } from '@ui/components/AppText';
 import { Button } from '@ui/components/Button';
+import { RadioGroup, RadioGroupIcon, RadioGroupItem, RadioGroupLabel } from '@ui/components/RadioGroup';
+import { theme } from '@ui/styles/theme';
+import { Step, StepContent, StepFooter, StepHeader, StepSubtitle, StepTitle } from '../components/Step';
 import { useOnboarding } from '../context/useOnboarding';
 
-export function GenderStep() {
-  const { currentStepIndex, nextStep, previousStep } = useOnboarding();
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppText size="3xl" weight="semiBold">Gender</AppText>
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE'
+}
 
-      <View>
-        <Button onPress={previousStep}>Voltar</Button>
-        <AppText>{currentStepIndex}</AppText>
-        <Button onPress={nextStep}>Avançar</Button>
-      </View>
-    </View>
+export function GenderStep() {
+  const { nextStep } = useOnboarding();
+  return (
+    <Step>
+      <StepHeader>
+        <StepTitle>Qual é seu gênero?</StepTitle>
+        <StepSubtitle>Seu gênero influencia no tipo de dieta</StepSubtitle>
+      </StepHeader>
+
+      <StepContent>
+        <RadioGroup orientation='horizontal'>
+          <RadioGroupItem value={Gender.MALE}>
+            <RadioGroupIcon>👨</RadioGroupIcon>
+            <RadioGroupLabel>Masculino</RadioGroupLabel>
+          </RadioGroupItem>
+          <RadioGroupItem value={Gender.FEMALE}>
+            <RadioGroupIcon>👩</RadioGroupIcon>
+            <RadioGroupLabel>Feminino</RadioGroupLabel>
+          </RadioGroupItem>
+        </RadioGroup>
+      </StepContent>
+
+      <StepFooter>
+        <Button size='icon' onPress={nextStep}>
+          <ArrowRightIcon size={20} color={theme.colors.black[700]} />
+        </Button>
+      </StepFooter>
+    </Step>
   );
 }
