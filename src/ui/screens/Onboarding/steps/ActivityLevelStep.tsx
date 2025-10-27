0bd8f04364fd161704edffc86a61935e7901f3cb
Ana Helena <anahelenarp@hotmail.com>
2025-10-27 08:54:11 -0300

    feat: activity level step

--- src/ui/screens/Onboarding/steps/ActivityLevelStep.tsx
+++ src/ui/screens/Onboarding/steps/ActivityLevelStep.tsx
@@ -1,21 +1,73 @@
-import { View } from 'react-native';
+import { ArrowRightIcon } from 'lucide-react-native';
+import React from 'react';
 
-import { AppText } from '@ui/components/AppText';
 import { Button } from '@ui/components/Button';
+import { RadioGroup, RadioGroupDescription, RadioGroupIcon, RadioGroupItem, RadioGroupItemInfo, RadioGroupLabel } from '@ui/components/RadioGroup';
+import { theme } from '@ui/styles/theme';
+import { Step, StepContent, StepFooter, StepHeader, StepTitle } from '../components/Step';
 import { useOnboarding } from '../context/useOnboarding';
 
+export enum ActivityLevel {
+  SEDENTARY = 'SEDENTARY',
+  LIGHT = 'LIGHT',
+  MODERATE = 'MODERATE',
+  HEAVY = 'HEAVY',
+  ATHLETE = 'ATHLETE'
+}
+
 export function ActivityLevelStep() {
-  const { currentStepIndex, nextStep, previousStep } = useOnboarding();
-
+  const { nextStep } = useOnboarding();
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppText size="3xl" weight="semiBold">ActivityLevelStep</AppText>
+    <Step>
+      <StepHeader>
+        <StepTitle>Qual é seu nível de atividade?</StepTitle>
+      </StepHeader>
 
-      <View>
-        <Button onPress={previousStep}>Voltar</Button>
-        <AppText>{currentStepIndex}</AppText>
-        <Button onPress={nextStep}>Avançar</Button>
-      </View>
-    </View>
+      <StepContent>
+        <RadioGroup>
+          <RadioGroupItem value={ActivityLevel.SEDENTARY}>
+            <RadioGroupIcon>🪑</RadioGroupIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupLabel>Sedentário</RadioGroupLabel>
+              <RadioGroupDescription>Não me exercito</RadioGroupDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+          <RadioGroupItem value={ActivityLevel.LIGHT}>
+            <RadioGroupIcon>🍃</RadioGroupIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupLabel>Leve</RadioGroupLabel>
+              <RadioGroupDescription>1 a 2 vezes por semana</RadioGroupDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+          <RadioGroupItem value={ActivityLevel.MODERATE}>
+            <RadioGroupIcon>⚡</RadioGroupIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupLabel>Moderado</RadioGroupLabel>
+              <RadioGroupDescription>3 a 5 vezes por semana</RadioGroupDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+          <RadioGroupItem value={ActivityLevel.HEAVY}>
+            <RadioGroupIcon>🔥</RadioGroupIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupLabel>Pesado</RadioGroupLabel>
+              <RadioGroupDescription>6 a 7 vezes por semana</RadioGroupDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+          <RadioGroupItem value={ActivityLevel.ATHLETE}>
+            <RadioGroupIcon>🏋️</RadioGroupIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupLabel>Atleta</RadioGroupLabel>
+              <RadioGroupDescription>2 vezes por dia</RadioGroupDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+        </RadioGroup>
+      </StepContent>
+
+      <StepFooter>
+        <Button size="icon" onPress={nextStep}>
+          <ArrowRightIcon size={20} color={theme.colors.black[700]} />
+        </Button>
+      </StepFooter>
+    </Step>
   );
 }
