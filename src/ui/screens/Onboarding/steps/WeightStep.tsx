f8d3def98416133f3915144d32466ee6ea7508ac
Ana Helena <anahelenarp@hotmail.com>
2025-10-27 08:26:41 -0300

    feat: weight step

--- src/ui/screens/Onboarding/steps/WeightStep.tsx
+++ src/ui/screens/Onboarding/steps/WeightStep.tsx
@@ -1,11 +1,35 @@
-import { View } from 'react-native';
+import { ArrowRightIcon } from 'lucide-react-native';
+import React from 'react';
 
-import { AppText } from '@ui/components/AppText';
+import { Button } from '@ui/components/Button';
+import { FormGroup } from '@ui/components/FormGroup';
+import { Input } from '@ui/components/Input';
+import { theme } from '@ui/styles/theme';
+import { Step, StepContent, StepFooter, StepHeader, StepSubtitle, StepTitle } from '../components/Step';
+import { useOnboarding } from '../context/useOnboarding';
 
 export function WeightStep() {
+  const { nextStep } = useOnboarding();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppText size="3xl" weight="semiBold">WeightStep</AppText>
-    </View>
+    <Step>
+      <StepHeader>
+        <StepTitle>Qual é seu peso?</StepTitle>
+        <StepSubtitle>Você pode inserir uma estimativa</StepSubtitle>
+      </StepHeader>
+
+      <StepContent position="center">
+        <FormGroup label="Peso" style={{ width: '100%' }}>
+          <Input placeholder="80" keyboardType="numeric" />
+        </FormGroup>
+      </StepContent>
+
+      <StepFooter>
+        <Button size="icon" onPress={nextStep}>
+          <ArrowRightIcon size={20} color={theme.colors.black[700]} />
+        </Button>
+      </StepFooter>
+    </Step>
   );
 }
+
