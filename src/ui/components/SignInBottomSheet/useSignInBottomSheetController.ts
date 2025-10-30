8393c1aa1ae3b562dad8921de1c5e649a0c9a967
Ana Helena <anahelenarp@hotmail.com>
2025-10-30 14:07:44 -0300

    feat: add signin schema

--- src/ui/components/SignInBottomSheet/useSignInBottomSheetController.ts
+++ src/ui/components/SignInBottomSheet/useSignInBottomSheetController.ts
@@ -1,27 +1,35 @@
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { zodResolver } from '@hookform/resolvers/zod';
 import React, { useImperativeHandle, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { Alert, TextInput } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { Alert, TextInput } from 'react-native';
 import { ISignInBottomSheet } from './ISignInBottomSheet';
+import { signInSchema } from './schema';
 
 export function useSignInBottomSheetController(ref: React.Ref<ISignInBottomSheet>) {
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
   const { bottom } = useSafeAreaInsets();
   const passwordInputRef = useRef<TextInput>(null);
 
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+  });
+
   useImperativeHandle(ref, () => ({
     open: () => bottomSheetModalRef.current?.present(),
   }), []);
 
-  function handleSubmit() {
-    Alert.alert('Enviando dados do form');
-  }
+  const handleSubmit = form.handleSubmit((formData) => {
+    Alert.alert('Enviando dados do form', JSON.stringify(formData));
+  });
 
   return {
     bottomSheetModalRef,
     bottom,
     passwordInputRef,
+    form,
     handleSubmit,
   };
 }
